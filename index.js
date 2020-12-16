191b05207c4b2ef13bbe324b0ea4418e5cbc8bfd
Olaf van Venetie <olafvv@Olafs-MacBook-Pro.local>
2020-12-16 16:01:06 +0100

    static files removed

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
 const express = require('express'); 
 const app = express();
-
-const server = require('http').createServer(app);
+const http = require('http')
+const server = http.createServer(app);
 const io = require('socket.io')(server, {
   cors: {
     origin: '*',
@@ -11,6 +10,7 @@ const io = require('socket.io')(server, {
 });
 
 io.on('connection', socket => {
+  console.log('client connected');
   socket.on('chat', (message) => {
     io.emit('chat', {message, id: socket.id});
   });
