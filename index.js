07677a8463d6b4e10600fd2f581d253f0714ff64
Olaf van Venetie <olafvv@Olafs-MacBook-Pro.local>
2020-12-16 15:59:07 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const express = require('express'); 
+const app = express();
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
+io.on('connection', socket => {
+  socket.on('chat', (message) => {
+    io.emit('chat', {message, id: socket.id});
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on: ', 3000);
+});
